1d66932e3ba6be735c1ef8ff3e43d951666717ac
maxidiazbattan <85640305+maxidiazbattan@users.noreply.github.com>
2022-03-12 14:29:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -390,13 +390,13 @@ def build_graph(ticker, periods):
     # df_mkt = df_mkt.dropna()
 
     if periods == '1mo':
-        df_mkt=pd.read_csv('/datasets/periods_1mo.csv', header=[0,1], index_col=0)
+        df_mkt=pd.read_csv('datasets/periods_1mo.csv', header=[0,1], index_col=0)
     elif periods == '3mo':
-        df_mkt=pd.read_csv('/datasets/periods_3mo.csv', header=[0,1], index_col=0)
+        df_mkt=pd.read_csv('datasets/periods_3mo.csv', header=[0,1], index_col=0)
     elif periods == '6mo':
-        df_mkt=pd.read_csv('/datasets/periods_6mo.csv', header=[0,1], index_col=0)
+        df_mkt=pd.read_csv('datasets/periods_6mo.csv', header=[0,1], index_col=0)
     else:
-        df_mkt=pd.read_csv('/datasets/periods_1y.csv', header=[0,1], index_col=0)
+        df_mkt=pd.read_csv('datasets/periods_1y.csv', header=[0,1], index_col=0)
 
 
     fig7 = go.Figure(data=[go.Candlestick(x=df_mkt.index,
